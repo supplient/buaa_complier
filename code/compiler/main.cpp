68d7dfb93852a1f4178d032417040bbfe9543651
supplient <437566830@qq.com>
2018-11-13 14:52:00 +0800

    Add GrammarAnalyzer 's test.

--- code/compiler/main.cpp
+++ code/compiler/main.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 void lexTest(string filename);
+void graTest(string filename);
 
 int main()
 {
-    lexTest("lex_test.txt");
+    graTest("gra_test.c0");
 
     cout << "Hello world!" << endl;
     return 0;
