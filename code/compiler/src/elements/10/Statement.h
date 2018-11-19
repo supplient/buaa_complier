555600351549a71983c6e4981c40eedc85bb05d3
supplient <437566830@qq.com>
2018-11-19 09:11:36 +0800

    Grammar: Add Statment's classes.

--- code/compiler/src/elements/10/Statement.h
+++ code/compiler/src/elements/10/Statement.h
@@ -9,4 +9,49 @@ class Statement: public Element
 
 };
 
+class IfStatement: public Statement
+{
+
+};
+
+class WhileStatement: public Statement
+{
+
+};
+
+class FuncCallStatement: public Statement
+{
+
+};
+
+class AssignStatement: public Statement
+{
+
+};
+
+class InputStatement: public Statement
+{
+
+};
+
+class OutputStatement: public Statement
+{
+
+};
+
+class ReturnStatement: public Statement
+{
+
+};
+
+class EmptyStatement: public Statement
+{
+
+};
+
+class BracedStatement: public Statement
+{
+
+};
+
 #endif//STATEMENT_H
