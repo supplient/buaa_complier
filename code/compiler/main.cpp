4d7d3845192cf20ce0268d97c8e11eab213f5b83
supplient <437566830@qq.com>
2018-12-03 08:41:55 +0800

    Modify.DAG: Start design.

--- code/compiler/main.cpp
+++ code/compiler/main.cpp
@@ -21,7 +21,7 @@ int main()
         //semTest("example.c0");
         //backTest("back_test.c0");
         //backTest("example.c0");
-        modiTest("example.c0");
+        modiTest("dag_test.c0");
 
         //graInterface();
 
